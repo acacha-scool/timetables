2a6ed4d8b07eb084b8f7d41fdb1b47e58037830a
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2017-09-06 12:48:55 +0200

    Sync

---
+++ src/helpers.php
@@ -0,0 +1,7 @@
+<?php
+
+if (! function_exists('seed_potential_lessons_from_curriculum')) {
+    function seed_potential_lessons_from_curriculum() {
+        //Traverse curriculum. It assumes curriculum data is seeded
+    }
+}
